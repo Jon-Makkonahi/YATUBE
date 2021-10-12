0e1d90b2d229640de925a3f37adf87ae1df14c24
Jon-Makkonahi <danila-bibik2017@yandex.ru>
2021-10-12 16:37:56 +0700

    FIX FINAL

--- yatube/posts/tests/test_routers.py
+++ yatube/posts/tests/test_routers.py
@@ -21,15 +21,15 @@ class PostsRoutersTests(TestCase):
     def test_routers_pages(self):
         cases = [
             ['index', [], '/'],
-            ['group_list',[SLUG], f'/group/{SLUG}/'],
-            ['profile',[USERNAME], f'/profile/{USERNAME}/'],
+            ['group_list', [SLUG], f'/group/{SLUG}/'],
+            ['profile', [USERNAME], f'/profile/{USERNAME}/'],
             ['post_detail', [self.post.pk], f'/posts/{ self.post.pk }/'],
             ['post_edit', [self.post.pk], f'/posts/{ self.post.pk }/edit/'],
             ['post_create', [], '/create/'],
             ['follow_index', [], '/follow/'],
             ['profile_follow', [USERNAME], f'/profile/{USERNAME}/follow/'],
-            ['profile_unfollow', [USERNAME],  f'/profile/{USERNAME}/unfollow/'],
+            ['profile_unfollow', [USERNAME],  f'/profile/{USERNAME}/unfollow/']
         ]
         for url1, args, url2 in cases:
             with self.subTest(url=url1, args=args):
-                self.assertEqual(reverse('posts:' + url1, args= args), url2)
+                self.assertEqual(reverse('posts:' + url1, args=args), url2)
